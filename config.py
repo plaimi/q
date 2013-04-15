0498f7019a1d6f2ee334f498afb080513c33d9b2
Alexander Berntsen <alexander@plaimi.net>
2013-04-15 15:29:08 +0200

    Add comments to configuration file

--- config.py
+++ config.py
@@ -1,9 +1,18 @@
+# IRC Network to connect to
 network = 'irc.freenode.net'
+# Port to connect on
 port = 6667
+# Channel to join (automatically)
 chan = 'bot_test'
+# Nickname of bot
 nickname = 'my_bot'
+# Username to use when identifying with services (or not)
 username = nickname
+# Whether to use a password to identify with services or not
 password = False
+# IRC nick names that can control the bot
 masters = [nickname, 'my_nickname']
+# High score database file (is automatically created)
 hiscoresdb = 'hiscores.sql'
+# Whether to print 'category - question - answer' to STDOUT
 verbose = True
